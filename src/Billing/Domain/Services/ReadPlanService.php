567f2056f90862c81a617d753f5c7044000bbe5b
zumber <84306702+zumberr@users.noreply.github.com>
2024-11-03 15:51:53 -0500

    Add files via upload

---
+++ src/Billing/Domain/Services/ReadPlanService.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Billing\Domain\Services;
+
+use Billing\Domain\Entities\PlanEntity;
+use Billing\Domain\Exceptions\PlanNotFoundException;
+use Billing\Domain\Repositories\PlanRepositoryInterface;
+use Shared\Domain\ValueObjects\Id;
+
+/**
+ * @package Plan\Domain\Services
+ */
+class ReadPlanService
+{
+    /**
+     * @param PlanRepositoryInterface $repo
+     * @return void
+     */
+    public function __construct(
+        private PlanRepositoryInterface $repo,
+    ) {
+    }
+
+    /**
+     * @param Id $id
+     * @return PlanEntity
+     * @throws PlanNotFoundException
+     */
+    public function findPlanOrFail(Id $id): PlanEntity
+    {
+        $plan = $this->repo->ofId($id);
+        if (null === $plan) {
+            throw new PlanNotFoundException($id);
+        }
+
+        return $plan;
+    }
+}
